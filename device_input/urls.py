905fd28f81f2e793dcc240f05715fed5856b98d1
Sylwedn <sylwedn@gmail.com>
2023-11-01 18:43:37 +0200

    filter input, only int and 8 numbers

--- device_input/urls.py
+++ device_input/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('dev_in/<int:dev_in_code>', views.dev_input),  # http://127.0.0.1:8000/dev_input/
+    re_path(r'^dev_in/(?P<dev_in_code>[0-9]{8})/', views.dev_input),  # http://127.0.0.1:8000/dev_input/
 ]
+
+# TODO: dev_in_code treatment for DB, and logic
+# TODO: Adapt to temp sensor when installed {8} to {10}
--- device_input/views.py
+++ device_input/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def dev_input(request, dev_in_code):
     return HttpResponse(f'<h1>Device input</h1> <p> code from device: {dev_in_code} </p>')
+
+# TODO: Decode input, for relay, endswith, temp variables
