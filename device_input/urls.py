5a6e5384c987fc7518d747a426791a97ad0ded76
Sylwedn <sylwedn@gmail.com>
2023-11-02 20:13:13 +0200

    First command successfully sent to Raspberry Pi Pico

--- device_input/urls.py
+++ device_input/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^dev_in/(?P<dev_in_code>[0-9]{8})/', views.dev_input),  # http://127.0.0.1:8000/dev_input/
+    re_path(r'^dev_in/(?P<dev_in_code>[0-9]{8})/', views.dev_input),  # http://127.0.0.1:8000/dev_inp/12345678
 ]
 
 # TODO: dev_in_code treatment for DB, and logic
