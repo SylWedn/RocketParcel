707ae1e3c217178d4ba968a1c0bfd6d6a435c3f4
Sylwedn <sylwedn@gmail.com>
2023-11-12 19:42:31 +0200

    after many tests with DB, started confining admin, firs action changed Django administration title to Rocket Parcel Admin

--- project_management/admin.py
+++ project_management/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from project_management.models import Packagedb
+
+admin.site.register(Packagedb)
--- project_management/templatetags/project_management_tags.py
+++ project_management/templatetags/project_management_tags.py
@@ -1,14 +1,12 @@
 from django import template
 import project_management.views as views
+from project_management.models import Category
 
 register = template.Library()
 
 
-@register.simple_tag(name='getcats')
-def get_categories():
-    return views.cats_db
 
 @register.inclusion_tag('list_categories.html')
-def show_categories(cat_selected=0):
-    cats = views.cats_db
-    return {'cats': cats, 'cat_selected': cat_selected}
+def show_categories(cat_selected_id=0):
+    cats = Category.objects.all()
+    return {"cats": cats, "cat_selected": cat_selected_id}
