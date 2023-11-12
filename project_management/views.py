a036cf02bb3fb118601f8fb1d33917538209ebba
Sylwedn <sylwedn@gmail.com>
2023-11-12 19:42:30 +0200

    after many tests with DB, started confining admin, firs action changed Django administration title to Rocket Parcel Admin

--- project_management/views.py
+++ project_management/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 
-from .models import Packagedb
+from .models import Packagedb, Category
 
 # Create your views here.
 
@@ -18,12 +18,6 @@ data_db = [
     {'id': 1, 'title': 'first article', 'content': 'text of first article', 'is_published': True},
     {'id': 2, 'title': 'second article', 'content': 'text of second article', 'is_published': False},
     {'id': 3, 'title': 'third article', 'content': 'text of third article', 'is_published': True}
-]
-
-cats_db = [
-    {'id': 1, 'name': 'Category nr 1'},
-    {'id': 2, 'name': 'Category nr 2'},
-    {'id': 3, 'name': 'Category nr 3'},
 ]
 
 
@@ -54,7 +48,6 @@ def show_post(request, post_slug):
     return render(request, 'post.html', context=data)
 
 
-
 def add_page(request):
     return HttpResponse("Add page")
 
@@ -66,11 +59,15 @@ def contact(request):
 def login(request):
     return HttpResponse("Login page")
 
-def show_category (request, cat_id):
+
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    posts = Packagedb.published.filter(cat_id=category.pk)
     data = {
-        'title': 'category title',
+        'title': f'Рубрика: {category.name}',
         'menu': menu,
-        'posts': data_db,
-        'cat_selected': cat_id,
+        'posts': posts,
+        'cat_selected': category.pk,
     }
+
     return render(request, 'index.html', context=data)
