634106f8d910bf5877511608a81ee8917cad0282
Sylwedn <sylwedn@gmail.com>
2023-11-13 21:48:45 +0200

    WIP Form to DB

--- project_management/forms.py
+++ project_management/forms.py
@@ -3,9 +3,9 @@ from .models import Category
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label='Title')
+    slug = forms.SlugField(max_length=255, label='URL')
+    content = forms.CharField(widget=forms.Textarea(), required=False, label='Content')
+    is_published = forms.BooleanField(label='Status')
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Category')
 
